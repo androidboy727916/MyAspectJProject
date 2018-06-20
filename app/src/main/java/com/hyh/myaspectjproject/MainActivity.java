3f52f8d5d817ca5398ce86c08719d267590a51ab
hyh <m15517208396@163.com>
2018-06-20 13:58:56 +0800

    添加申请权限回掉

--- app/src/main/java/com/hyh/myaspectjproject/MainActivity.java
+++ app/src/main/java/com/hyh/myaspectjproject/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hyh.myaspectjproject;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
 import com.xdja.permissionlibrary.annotation.HuPermission;
@@ -14,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * 这是申请权限是否成功回掉
+     * @param requestCode
+     * @param permissions
+     * @param grantResults 不等于0就是没有申请成功
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
+    }
 }
